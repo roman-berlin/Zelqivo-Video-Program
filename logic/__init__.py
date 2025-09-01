ac2f0d0008f0a5501a21e279f78efaf067295285
Roman Berlin <romario.berlin@gmail.com>
2025-09-02 00:04:43 +0300

    big archetecture changing

--- logic/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core processing logic for the Active Speaker Video Merger.
-
-This package contains modules responsible for heavy operations such as
-active speaker detection, audio synchronisation and video merging.
-"""
-from .processing_pipeline import ProcessingPipeline  # noqa: F401
--- ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""User interface package.
-
-This package contains all PyQt6 widgets and windows for the application.
-It is organised into modules for each custom widget and window.
-"""
-from .main_window import MainWindow  # noqa: F401
